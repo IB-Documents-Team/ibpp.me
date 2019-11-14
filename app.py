5017ca9a913d36a5af8b781ef36284160aa6f3b3
vardy <jarred.vardy@gmail.com>
2019-11-14 18:19:54 +0800

    Fixed project name in license header

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ Copyright 2019 Jarred Vardy
 
 This file is part of ibpp.me.
 
-IB.ai is free software: you can redistribute it and/or modify it
+ibpp.me is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-IB.ai is distributed in the hope that it will be useful, but
+ibpp.me is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with IB.ai. If not, see http://www.gnu.org/licenses/.
+along with ibpp.me. If not, see http://www.gnu.org/licenses/.
 """
 
 import os
